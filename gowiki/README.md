a5445291a3315bd2a80891288988d28a1be940c9
zen <zen@zen>
2021-05-25 21:18:39 -0700

    use html templates

--- gowiki/README.md
+++ gowiki/README.md
@@ -2,3 +2,4 @@ https://golang.org/doc/articles/wiki/
 
 1. create a data structure with load and save methods
 2. use the net/http package to build web applications
+3. use the html/template package to process HTML templates
--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -5,11 +5,12 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 //describes how the wiki page will be stored in memory
 type page struct {
-	title string
+	Title string
 	body  []byte // byte slice rather than string as this is the type expected by the io libraries
 }
 
@@ -20,34 +21,34 @@ if there is no error it returns nil (the zero-value for pointers, interfaces, an
 octal integer literal 0600, indicates that the file should be created with read-write permissions for the current user only.
 */
 func (p *page) save() error {
-	f := p.title + ".txt"
+	f := p.Title + ".txt"
 	return ioutil.WriteFile(f, p.body, 0600)
 }
 
 // why not a method? tutorial has a function - May 22
-func loadPage(title string) (*page, error) {
-	f := title + ".txt"
+func loadPage(Title string) (*page, error) {
+	f := Title + ".txt"
 	body, err := ioutil.ReadFile(f)
 	if err != nil {
 		return nil, err
 	}
-	return &page{title: title, body: body}, nil
+	return &page{Title: Title, body: body}, nil
 }
 
 // load method not in the tutorial - May 22
 func (p *page) load() (*page, error) {
-	f := p.title + ".txt"
+	f := p.Title + ".txt"
 	body, err := ioutil.ReadFile(f)
 	if err != nil {
 		return nil, err
 	}
-	return &page{title: p.title, body: body}, nil
+	return &page{Title: p.Title, body: body}, nil
 
 }
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	/*
 		var p1 page
-		p1.title = r.URL.Path[len("/view/"):]
+		p1.Title = r.URL.Path[len("/view/"):]
 		p, err := p1.load()
 	*/
 
@@ -57,7 +58,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln(err)
 	}
 	//fmt.Fprintln(w, string(p2.body))
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.title, p.body)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.body)
 
 }
 
@@ -65,7 +66,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 //(or, if it doesn't exist, create an empty Page struct), and displays an HTML form.
 func editHandler1(w http.ResponseWriter, r *http.Request) {
 	t := r.URL.Path[len("/edit1/"):]
-	p1 := &page{title: t}
+	p1 := &page{Title: t}
 	p2, err := p1.load()
 
 	if err != nil {
@@ -74,14 +75,14 @@ func editHandler1(w http.ResponseWriter, r *http.Request) {
 			"<textarea name=\"body\">%s</textarea><br>"+
 			"<input type=\"submit\" value=\"Save\">"+
 			"</form>",
-			p1.title, p1.title, p1.body)
+			p1.Title, p1.Title, p1.body)
 	} else {
 		fmt.Fprintf(w, "<h1>editing %s</h1>"+
 			"<form action=\"/save/%s\" method=\"POST\">"+
 			"<textarea name=\"body\">%s</textarea><br>"+
 			"<input type=\"submit\" value=\"Save\">"+
 			"</form>",
-			p2.title, p2.title, p2.body)
+			p2.Title, p2.Title, p2.body)
 	}
 	fmt.Fprintf(w, "<h6>func (p *page) load() (*page, error) has been used</h6>")
 }
@@ -91,24 +92,23 @@ func editHandler2(w http.ResponseWriter, r *http.Request) {
 	t := r.URL.Path[len("/edit2/"):]
 	p, err := loadPage(t)
 	if err != nil {
-		p = &page{title: t}
+		p = &page{Title: t}
 	}
 
-	fmt.Fprintf(w, "<h1>editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</textarea><br>"+
-		"<input type=\"submit\" value=\"Save\">"+
-		"</form>",
-		p.title, p.title, p.body)
-	fmt.Fprintf(w, "<h6>func loadPage(title string) (*page, error) has been used</h6>")
+	// will read the contents of edit.html and return a *template.Template.
+	templ, _ := template.ParseFiles("edit.html")
+	// executes the template, writing the generated HTML to the http.ResponseWriter
+	templ.Execute(w, p)
+
+	//fmt.Fprintf(w, "<h6>func loadPage(Title string) (*page, error) has been used</h6>")
 }
 
 func main() {
 
-	p1 := &page{title: "test", body: []byte("this is a test page")}
+	p1 := &page{Title: "test", body: []byte("this is a test page")}
 	p1.save()
 
-	//p2, err := loadPage(p1.title)
+	//p2, err := loadPage(p1.Title)
 	p2, err := p1.load()
 	if err != nil {
 		log.Fatalln(err)
