7c9f5b210192c15e7cce7bb2dcfa32deb8b12409
zen <zen@zen>
2022-04-10 18:27:50 -0700

    chi, a lightweight, idiomatic and composable router for building Go HTTP services

--- chi/hello/main.go
+++ chi/hello/main.go
@@ -4,13 +4,16 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	//"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const port string = ":8080"
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Get("/en", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
